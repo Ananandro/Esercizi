304f4bc10fbc74321a66c28dc863d314d9dd3b26
Moschini Andrea (ACC0144089) <andrea.moschini@sci-b106pc079x.unitn.it>
2020-10-15 14:51:12 +0200

    okokok

---
+++ esercizio 0.9/roba_ascii.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int i;
+char x;
+int z;
+char y;
+int u;
+char p;
+
+
+int main(){
+
+for(i=65; i<=90; i++){
+
+    x = i;
+    cout << "CHAR: " << x << "\t ASCII:" << i << "\n";
+
+    }
+for(z=97; z<=122; z++){
+
+    y = z;
+    cout << "CHAR: " << y << "\t ASCII:" << z << "\n";
+
+    }
+for(u=48; u<=57; u++){
+
+    p = u;
+    cout << "CHAR: " << p << "\t ASCII:" << u << "\n";
+
+    }
+}
